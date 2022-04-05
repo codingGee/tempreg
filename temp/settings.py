665e64625eb68681f568fafc95d1e0149594960a
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-04-05 17:24:50 +0100

    updated settings.py

--- temp/settings.py
+++ temp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-6kwm69vcs&_%_r^nl$-$3=4$y+seszqx(%%muabtq%rmp38%4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tempreg.ng']
 
 # Application definition
 
