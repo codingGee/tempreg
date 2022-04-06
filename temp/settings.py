071e412ef8e9524eb8dfb89e8218d81075bf4182
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-04-06 08:55:47 +0100

    reinstalled whitenoise

--- temp/settings.py
+++ temp/settings.py
@@ -117,7 +117,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
